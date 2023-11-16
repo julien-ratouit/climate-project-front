a533f326f7e9b3675c3a6d4ef7b032574878e2ce
julien ratouit <77002747+julien-ratouit@users.noreply.github.com>
2023-11-16 16:17:47 +0100

    departements

--- climate-project-front/src/app/app.component.ts
+++ climate-project-front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RecupMeteoService } from './services/recup-meteo.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'climate-project-front';
+
+  isLoaded = false;
+  message: any;
+
+  constructor(private meteoService: RecupMeteoService) {}
+
+  ngOnInit(): void {
+    this.meteoService.getMeteo().subscribe((rep: any) => {
+      console.log(rep);
+      this.message = rep;
+      this.isLoaded = true;
+    });
+  }
 }
---
+++ climate-project-front/src/app/services/recup-meteo.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecupMeteoService {
+  private url = "http://127.0.0.1:5000/";
+  private endpoint = "departements";
+
+  constructor(private http: HttpClient) {}
+
+  getMeteo(){
+    var header = new HttpHeaders()
+    .set('Access-Control-Allow-Origin', 'http://127.0.0.1:4200/')
+    .set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+    .set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+    return this.http.get(this.url + this.endpoint, {'headers': header});
+  }
+
+}
